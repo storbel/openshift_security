e34dc0b729703053f4575aeda827f42db0be0b12
storbel <contact@storbel.com>
2023-09-03 00:44:50 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -10,6 +10,23 @@ $otherApps = [
 ];
 
 unset($otherApps[$appName]); 
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['city'])) {
+    $city = $_POST['city'];
+    $apiKey = getenv('WEATHER_API_KEY');  // Fetch the API key from environment variable
+    $apiUrl = "http://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$apiKey}&units=metric";
+    $weatherData = file_get_contents($apiUrl);
+    $weatherArray = json_decode($weatherData, true);
+    if ($weatherArray['cod'] == 200) {
+        $weather = "The current temperature in " . $city . " is " . $weatherArray['main']['temp'] . "°C.";
+    } else {
+        $weather = "Couldn't retrieve the temperature for " . $city . ". Please make sure the city's name is correct.";
+    }
+}
+
+
+
+    
 ?>
 
 <!DOCTYPE html>
@@ -42,6 +59,24 @@ unset($otherApps[$appName]);
     </div>
 </div>
 
+
+<div class="container mt-5">
+    <!-- Weather form -->
+    <div class="mb-4">
+        <form action="" method="post" class="form-inline">
+            <input type="text" name="city" class="form-control mr-2" placeholder="Enter city name">
+            <button type="submit" class="btn btn-primary">Get Temperature</button>
+        </form>
+        <?php if ($weather): ?>
+            <p class="mt-3"><?php echo $weather; ?></p>
+        <?php endif; ?>
+    </div>
+    
+    <!-- The rest of the application status content -->
+
+</div>
+
+    
 <!-- Optional JavaScript -->
 <!-- jQuery, Popper.js, and Bootstrap JS -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
